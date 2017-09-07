91455a2a4b5d046aa844c93975e47816d9e0505f
unknown <hoangduccuongqb@gmail.com>
2017-09-07 16:28:13 +0700

    updata 7/9/2017

---
+++ app/src/main/java/com/example/fullstack/apppuzzle/AdpterPlayPuzzle.kt
@@ -0,0 +1,5 @@
+package com.example.fullstack.apppuzzle
+
+/**
+ * Created by FullStack on 9/7/2017.
+ */
---
+++ app/src/main/java/com/example/fullstack/apppuzzle/PlayGamePuzzleActivity.kt
@@ -0,0 +1,12 @@
+package com.example.fullstack.apppuzzle
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class PlayGamePuzzleActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.layout_play_game_puzzle)
+    }
+}
